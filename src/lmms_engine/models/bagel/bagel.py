c459d51e8d17ac77566cc072ac10215ae673dabf
pufanyi <FPU001@e.ntu.edu.sg>
2025-09-06 14:19:32 +0800

    update

--- src/lmms_engine/models/bagel/bagel.py
+++ src/lmms_engine/models/bagel/bagel.py
@@ -380,6 +380,7 @@ class Bagel(PreTrainedModel):
                 packed_latent.append(latent)
             packed_latent_clean = torch.cat(packed_latent, dim=0)
 
+            torch.manual_seed(42)
             noise = torch.randn_like(packed_latent_clean)
             packed_timesteps = torch.sigmoid(packed_timesteps)
             packed_timesteps = (
