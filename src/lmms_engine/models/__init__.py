3f7f870bb5f485937538333372cffb1b63ae1f60
kcz358 <kaichenzhang358@outlook.com>
2025-09-18 02:14:52 -0700

    Qwen3 moe import

--- src/lmms_engine/models/__init__.py
+++ src/lmms_engine/models/__init__.py
@@ -15,6 +15,7 @@ from .qwen3_dllm import Qwen3DLLMConfig, Qwen3DLLMForMaskedLM
 from .qwen3_vl import apply_liger_kernel_to_qwen3_vl
 from .rae_siglip import RaeSiglipConfig, RaeSiglipModel
 from .sit import SiT, SiTConfig, SiTModel
+from .qwen3_moe import apply_liger_kernel_to_qwen3_moe
 from .wanvideo import (
     WanVideoConfig,
     WanVideoForConditionalGeneration,
@@ -36,6 +37,7 @@ __all__ = [
     "apply_liger_kernel_to_qwen2_5_vl",
     "apply_liger_kernel_to_qwen2_audio",
     "apply_liger_kernel_to_qwen3_vl",
+    "apply_liger_kernel_to_qwen3_moe",
     "WanVideoConfig",
     "WanVideoForConditionalGeneration",
     "WanVideoProcessor",
