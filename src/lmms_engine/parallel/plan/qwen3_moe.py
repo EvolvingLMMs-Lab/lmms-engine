d4ae885b8e5f72ca3fbe5d49f60a71f1f66c0c0b
kcz358 <kaichenzhang358@outlook.com>
2025-09-19 06:25:34 -0700

    Prepare module input for qwen3 moe

--- src/lmms_engine/parallel/plan/qwen3_moe.py
+++ src/lmms_engine/parallel/plan/qwen3_moe.py
@@ -1,10 +1,19 @@
+import torch
 import torch.nn as nn
 from loguru import logger
 from torch.distributed.device_mesh import DeviceMesh
-from torch.distributed.tensor.parallel import PrepareModuleInput, parallelize_module
+from torch.distributed.tensor import DTensor, Replicate
+from torch.distributed.tensor.parallel import (
+    ParallelStyle,
+    PrepareModuleInput,
+    PrepareModuleInputOutput,
+    parallelize_module,
+)
 from transformers import Qwen3MoeForCausalLM
 from transformers.models.qwen3_moe.modeling_qwen3_moe import (
-    Qwen3MoeDecoderLayer,
+    Qwen3MoeAttention,
+    Qwen3MoeMLP,
+    Qwen3MoeRMSNorm,
     Qwen3MoeSparseMoeBlock,
 )
 
@@ -27,5 +36,51 @@ def apply_qwen3_moe_parallel(
             device_mesh=ep_mesh,
             parallelize_plan=ep_plan,
         )
+
+    for name, module in model.model.named_modules():
+        if isinstance(module, Qwen3MoeSparseMoeBlock):
+            parallel_style = PrepareModuleInput(
+                input_layouts=Replicate(),
+                desired_input_layouts=Replicate(),
+                use_local_output=True,
+            )
+            parallelize_module(
+                module,
+                device_mesh=ep_mesh,
+                parallelize_plan=parallel_style,
+            )
+        if isinstance(module, Qwen3MoeAttention):
+            attention_parallel_style = PrepareModuleInputOutput(
+                input_kwarg_layouts={
+                    "hidden_states": Replicate(),
+                    "position_embeddings": Replicate(),
+                },
+                desired_input_kwarg_layouts={
+                    "hidden_states": Replicate(),
+                    "position_embeddings": Replicate(),
+                },
+                output_layouts=(Replicate(), None),
+                desired_output_layouts=(Replicate(), None),
+                use_local_output=True,
+            )
+        # No need to prepare input for the norm layer in model
+        if (
+            isinstance(module, nn.Linear)
+            or isinstance(module, Qwen3MoeMLP)
+            or isinstance(module, Qwen3MoeRMSNorm)
+            and name != "norm"
+        ):
+            linear_parallel_style = PrepareModuleInputOutput(
+                input_layouts=Replicate(),
+                desired_input_layouts=Replicate(),
+                output_layouts=Replicate(),
+                desired_output_layouts=Replicate(),
+                use_local_output=True,
+            )
+            parallelize_module(
+                module,
+                device_mesh=ep_mesh,
+                parallelize_plan=linear_parallel_style,
+            )
     logger.info(f"Applied Qwen3MoeParallelStyle to {len(model.model.layers)} layers")
     logger.info(f"Model: {model}")
