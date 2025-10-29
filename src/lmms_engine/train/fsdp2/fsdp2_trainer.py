b5baa1a9045f04c1a57db3458429534a1f8e7698
kcz358 <kaichenzhang358@outlook.com>
2025-10-28 19:29:00 -0700

    rfc the apply parallelize into fsdp2 trainer part

--- src/lmms_engine/train/fsdp2/fsdp2_trainer.py
+++ src/lmms_engine/train/fsdp2/fsdp2_trainer.py
@@ -21,6 +21,7 @@ from transformers.trainer_utils import seed_worker
 
 import lmms_engine.models.utils as model_utils
 import lmms_engine.parallel.process_group_manager as pgm
+from lmms_engine.parallel.parallelize import MODEL_TO_PARALLEL_METHOD, apply_parallelize
 from lmms_engine.train.config import TrainingArguments
 from lmms_engine.train.registry import TRAINER_REGISTER
 from lmms_engine.utils import TrainUtilities
@@ -110,6 +111,17 @@ class FSDP2SFTTrainer:
         return dataloader
 
     def prepare_model(self):
+        model_type = getattr(self.model.config, "model_type", None)
+        if model_type in MODEL_TO_PARALLEL_METHOD:
+            full_state = self.model.state_dict()
+            apply_parallelize(self.model, model_type, self.args)
+            fsdp2_load_full_state_dict(self.model, full_state)
+            self.fsdp2_model = self.model
+            del full_state
+            gc.collect()
+            torch.cuda.empty_cache()
+            return
+
         if self.args.bf16:
             param_dtype = torch.bfloat16
         else:
