c2a2067ec4048001c9a37e73f979f8c43f25222d
Jinghao-Guo <talentkkggg@outlook.com>
2025-10-30 17:43:01 +0800

    feat: Implement Qwen3MoeExperts class and integrate into parallelization workflow

--- src/lmms_engine/parallel/qwen3_moe/style.py
+++ src/lmms_engine/parallel/qwen3_moe/style.py
@@ -2,6 +2,7 @@ from functools import partial
 from typing import Optional
 
 import torch
+import torch.distributed as dist
 import torch.nn as nn
 from torch.distributed.tensor import (
     DeviceMesh,
@@ -13,7 +14,14 @@ from torch.distributed.tensor import (
 )
 from torch.distributed.tensor.parallel import ParallelStyle
 from torch.distributed.tensor.placement_types import Placement
-from transformers.models.qwen3_moe.modeling_qwen3_moe import Qwen3MoeSparseMoeBlock
+
+import lmms_engine.parallel.process_group_manager as pgm
+from lmms_engine.models.qwen3_moe.qwen3_moe_experts import Qwen3MoeExperts
+from lmms_engine.parallel.expert_parallel.utils import (
+    _compute_permute_indices,
+    _token_combine,
+    _token_dispatch,
+)
 
 
 class Qwen3MoeParallelStyle(ParallelStyle):
@@ -28,10 +36,50 @@ class Qwen3MoeParallelStyle(ParallelStyle):
         self.output_layouts = (output_layouts or Shard(0),)
         self.use_local_output = use_local_output
         self.desired_input_layouts = (Shard(0),)
+        self.input_splits = None
+        self.output_splits = None
+
+    def _input_fn(self, inputs, mesh: DeviceMesh):
+        routed_input, num_tokens_per_expert = inputs
+        if pgm.process_group_manager.ep_world_size > 1:
+            (
+                routed_input,
+                input_splits,
+                output_splits,
+                num_tokens_per_expert_group,
+            ) = _token_dispatch(routed_input, num_tokens_per_expert)
+            permute_indices, split_sizes = _compute_permute_indices(
+                torch.tensor(num_tokens_per_expert_group, device=routed_input.device),
+                pgm.process_group_manager.ep_world_size,
+                self.num_experts // pgm.process_group_manager.ep_world_size,
+            )
+            routed_input = routed_input[permute_indices]
+            routed_input = torch.split(
+                routed_input[: sum(output_splits)],
+                split_size_or_sections=split_sizes,
+                dim=0,
+            )
+            self.input_splits = input_splits
+            self.output_splits = output_splits
+            self.permute_indices = permute_indices
+
+        else:
+            routed_input = torch.split(
+                routed_input,
+                split_size_or_sections=num_tokens_per_expert.tolist(),
+                dim=0,
+            )
+        return routed_input
+
+    def _output_fn(self, output, mesh: DeviceMesh):
+        if pgm.process_group_manager.ep_world_size > 1:
+            output[self.permute_indices] = output.clone()
+            output = _token_combine(output, self.input_splits, self.output_splits)
+        return output
 
     @staticmethod
     def _partition_fn(name, mod, device_mesh):
-        if isinstance(mod, Qwen3MoeSparseMoeBlock):
+        if isinstance(mod, Qwen3MoeExperts):
             # Distribute the expert parameters across the expert parallel mesh
             expert_parallel_dim = 0  # Assuming experts are sharded along the first dimension
 
@@ -70,9 +118,9 @@ class Qwen3MoeParallelStyle(ParallelStyle):
     # Check the lmms_engine/models/qwen3_moe/qwen3_moe_ops.py
     # Since the expert module is a module list, we don't define the token
     # dispatch here
-    @staticmethod
-    def _input_fn(input_layouts, desired_input_layouts, mod, input, device_mesh):
-        return input
+    # @staticmethod
+    # def _input_fn(input_layouts, desired_input_layouts, mod, input, device_mesh):
+    #     return input
 
     #  if not isinstance(input, tuple):
     #      input = (input,)
@@ -92,27 +140,19 @@ class Qwen3MoeParallelStyle(ParallelStyle):
     #      new_input.append(input_tensor)
     #  return tuple(new_input)
 
-    @staticmethod
-    def _output_fn(output_layouts, use_local_output, mod, output, device_mesh):
-        if isinstance(output, DTensor):
-            output = output.redistribute(placements=output_layouts, async_op=True)
-            if use_local_output:
-                output = output.to_local()
-        return output
+    # @staticmethod
+    # def _output_fn(output_layouts, use_local_output, mod, output, device_mesh):
+    #     if isinstance(output, DTensor):
+    #         output = output.redistribute(placements=output_layouts, async_op=True)
+    #         if use_local_output:
+    #             output = output.to_local()
+    #     return output
 
     def _apply(self, module: nn.Module, device_mesh: DeviceMesh) -> nn.Module:
         return distribute_module(
             module,
             device_mesh,
             partition_fn=Qwen3MoeParallelStyle._partition_fn,
-            input_fn=partial(
-                Qwen3MoeParallelStyle._input_fn,
-                self.input_layouts,
-                self.desired_input_layouts,
-            ),
-            output_fn=partial(
-                Qwen3MoeParallelStyle._output_fn,
-                self.output_layouts,
-                self.use_local_output,
-            ),
+            input_fn=Qwen3MoeParallelStyle._input_fn,
+            output_fn=Qwen3MoeParallelStyle._output_fn,
         )
