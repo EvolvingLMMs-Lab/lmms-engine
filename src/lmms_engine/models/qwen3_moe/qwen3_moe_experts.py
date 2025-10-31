20224c9ce3aa0b6431be0e7f27f1c190aa054d53
kcz358 <kaichenzhang358@outlook.com>
2025-10-31 09:18:02 +0000

    if else for converting local weights

--- src/lmms_engine/models/qwen3_moe/qwen3_moe_experts.py
+++ src/lmms_engine/models/qwen3_moe/qwen3_moe_experts.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from torch.distributed.tensor import DTensor
+
 
 class Qwen3MoeExperts(nn.Module):
     def __init__(self, num_experts: int, hidden_dim: int, intermediate_size: int, act_fn):
@@ -18,11 +20,18 @@ class Qwen3MoeExperts(nn.Module):
         )
         self.num_experts = num_experts
         self.act_fn = act_fn
+
     def forward(self, *routed_input):
         out_experts_split = []
-        down_proj = self.down_proj.to_local()
-        up_proj = self.up_proj.to_local()
-        gate_proj = self.gate_proj.to_local()
+        if isinstance(self.down_proj, DTensor):
+            down_proj = self.down_proj.to_local()
+            up_proj = self.up_proj.to_local()
+            gate_proj = self.gate_proj.to_local()
+        else:
+            down_proj = self.down_proj
+            up_proj = self.up_proj
+            gate_proj = self.gate_proj
+
         for idx, x in enumerate(routed_input):
             hidden = self.act_fn(torch.matmul(x, gate_proj[idx].transpose(-2, -1)))
             hidden = hidden * torch.matmul(x, up_proj[idx].transpose(-2, -1))
