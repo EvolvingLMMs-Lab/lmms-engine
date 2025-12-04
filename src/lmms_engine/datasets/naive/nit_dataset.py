401df5f9358893b1a6028702a638ca945b411c23
pufanyi <pufanyi@sensetime.com>
2025-12-04 17:29:22 +0800

    refactor: reorder import statement and remove extraneous blank lines.

--- src/lmms_engine/datasets/naive/nit_dataset.py
+++ src/lmms_engine/datasets/naive/nit_dataset.py
@@ -1,20 +1,22 @@
+from datasets import load_dataset
+
 from lmms_engine.datasets.config import DatasetConfig
 from lmms_engine.mapping_func import register_dataset
-from datasets import load_dataset
+
 
 @register_dataset("nit")
 class NitDataset:
     def __init__(self, config: DatasetConfig) -> None:
         self.config = config
         self.processor = NitDataProcessor(config)
-    
+
     def _build_from_config(self):
         # A bit ugly, but it seems that I cannot merge with multimodal dataset
         if self.config.dataset_format == "hf_dataset":
             self.dataset = load_dataset(self.config.dataset_path, split="train")
         else:
             raise NotImplementedError("Only hf_dataset is supported for now")
-        
+
         self.dataset = self.dataset.map(self.processor.process, num_proc=self.config.processor_workers)
 
     def __getitem__(self, index):
@@ -22,4 +24,3 @@ class NitDataset:
 
     def __len__(self):
         return len(self.dataset)
-
