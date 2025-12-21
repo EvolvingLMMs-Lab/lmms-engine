050a093ccc5128214ad4938f876803b00df3978a
KemingWu <wukemingcqu@gmail.com>
2025-12-21 07:24:29 +0000

    update config

--- src/lmms_engine/train/config.py
+++ src/lmms_engine/train/config.py
@@ -41,7 +41,7 @@ class TrainingArguments(transformers.TrainingArguments):
 
 @dataclass
 class TrainerConfig:
-    trainer_type: Literal["hf_trainer", "fsdp2_trainer", "bagel_grpo_trainer"]
+    trainer_type: Literal["hf_trainer", "fsdp2_trainer"]
     dataset_config: DatasetConfig
     trainer_args: TrainingArguments
     model_config: ModelConfig
