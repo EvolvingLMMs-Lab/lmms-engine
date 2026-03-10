53bbed68e5fdbb683c0ff9302e386a9ef625d2ce
mwxely <yang0756@e.ntu.edu.sg>
2026-03-10 18:04:34 +0800

    fix(processor): remove duplicate special_tokens property in qwen2_vl_processor

--- src/lmms_engine/datasets/processor/qwen2_vl_processor.py
+++ src/lmms_engine/datasets/processor/qwen2_vl_processor.py
@@ -29,15 +29,6 @@ class Qwen2VLDataProcessor:
                 self._special_tokens = list(self.processor.tokenizer.additional_special_tokens)
         return self._special_tokens
 
-    @property
-    def special_tokens(self):
-        if not hasattr(self, "_special_tokens"):
-            if hasattr(self.processor.tokenizer, "all_special_tokens"):
-                self._special_tokens = list(self.processor.tokenizer.all_special_tokens)
-            else:
-                self._special_tokens = list(self.processor.tokenizer.additional_special_tokens)
-        return self._special_tokens
-
     def _build_processor(self):
         processor = Qwen2VLProcessor.from_pretrained(self.config.processor_name)
         return processor
